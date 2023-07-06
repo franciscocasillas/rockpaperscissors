5b161fc9e4349efa747a812e2d6b23dd0027d7d2
Francisco J. Casillas <chiccasillas@gmail.com>
2023-07-06 12:51:11 -0600

    Get random generated computer choice

--- main.js
+++ main.js
@@ -1,53 +1,10 @@
 function getComputerChoice() {
-	let computerChoice = Math.floor(Math.random() * 10);
-	if (computerChoice === 0) {
-		getComputerChoice();
-		return;
-	} else if (computerChoice < 4) {
-		computerChoice = "rock";
-	} else if (computerChoice < 7) {
-		computerChoice = "paper";
+	let randomNumber = Math.ceil(Math.random() * 9);
+	if (randomNumber < 4) {
+		return "rock";
+	} else if (randomNumber < 7) {
+		return "paper";
 	} else {
-		computerChoice = "scissors";
-	}
-	return computerChoice;
-}
-
-function playRound(playerSelection) {
-	let computerSelection = getComputerChoice();
-	playerSelection = playerSelection.toLowerCase();
-	if (playerSelection === computerSelection) {
-		console.log(`You picked ${playerSelection}. 
-        The computer chose ${computerSelection}.
-        You TIED!`);
-		return "tie";
-	} else if (
-		(playerSelection === "rock" && computerSelection === "scissors") ||
-		(playerSelection === "paper" && computerSelection === "rock") ||
-		(playerSelection === "scissors" && computerSelection === "paper")
-	) {
-		console.log(`You picked ${playerSelection}. 
-        The computer chose ${computerSelection}.
-        You WON!`);
-		return "win";
-	} else {
-		console.log(`You picked ${playerSelection}. 
-        The computer chose ${computerSelection}.
-        You LOST!`);
-		return "lose";
-	}
-}
-
-function playGame(playerSelection) {
-	let userScore = 0;
-	let computerScore = 0;
-	while (userScore + computerScore < 5) {
-		let winner = playRound(playerSelection);
-		if (winner === "win") {
-			userScore += 1;
-		} else if (winner === "lose") {
-			computerScore += 1;
-		}
-		console.log(`${userScore} ${computerScore}`);
+		return "scissors";
 	}
 }
